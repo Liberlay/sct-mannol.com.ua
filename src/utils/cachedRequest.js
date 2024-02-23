fa84b2107f93579368e172a5a2ffcdebf6c7c2cd
Danyil <liberlay555@gmail.com>
2024-02-23 02:27:46 +0200

    Fixed cached request

--- src/utils/cachedRequest.js
+++ src/utils/cachedRequest.js
@@ -1,5 +1,5 @@
 export const cachedRequest = async (request, revalidate) => {
-  return fetch(`${process.env.HOST}api/v1/${request}`, {
+  return fetch(`${process.env.HOST}api/v1${request}`, {
     next: { revalidate },
   }).then((res) => res.json())
 }
